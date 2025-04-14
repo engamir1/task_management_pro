1829620e9c07807f13a1cb9d9e4d0983c648352b
engamir1 <engmohamed.amir101@gmail.com>
2025-04-14 04:11:02 +0200

    add tailwind and syncfusion

--- task_manage_app/src/App.js
+++ task_manage_app/src/App.js
@@ -1,37 +1,88 @@
-import React from 'react';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import KanbanShowcase from './components/KanbanShowcase';
-import Testimonials from './components/Testimonials';
-import Pricing from './components/Pricing';
-import CTA from './components/CTA';
-import Footer from './components/Footer';
-import MainBoard from './components/MainBoard';
-import Home from './components/Home';
-import Login from './components/Login';
-
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainBoard from "./components/MainBoard";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 const App = () => {
+  const activeMenu = false;
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/main" element={<MainBoard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>);
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {/* side menu  */}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Side BAr
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Side bar </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            } `}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              NavBar
+            </div>
+            {/* navigation bar  */}
+          </div>
+          <div>
+            <Routes>
+              {/* this is dashboards  */}
+              <Route path="/" element="Ecommerce"></Route>
+              <Route path="/ecommerce" element="Ecommerce"></Route>
+              {/* pages */}
+              <Route path="/orders" element="Orders"></Route>
+              <Route path="/employees" element="Emp"></Route>
+              <Route path="/customers" element="Customers"></Route>
+              {/* Apps  */}
+              <Route path="/kanban" element="Customers"></Route>
+              <Route path="/editor" element="Customers"></Route>
+              <Route path="/calender" element="Customers"></Route>
+              <Route path="/color-picker" element="Customers"></Route>
+              {/* charts */}
+              <Route path="/line" element="Customers"></Route>
+              <Route path="/area" element="Customers"></Route>
+              <Route path="/bar" element="Customers"></Route>
+              <Route path="/pie" element="Customers"></Route>
+              <Route path="/financial" element="Customers"></Route>
+              <Route path="/color-mapping" element="Customers"></Route>
+              <Route path="/pyramids" element="Customers"></Route>
+              <Route path="/stacked" element="Customers"></Route>
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+
+    // <BrowserRouter>
+    //   <Header />
+    //   <h1 className="underline text-4xl"> Hello Task Manager</h1>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/main" element={<MainBoard />} />
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
+  );
 };
 
 export default App;
-
-
-
-
-        {/* <Route path="/features" element={<Features />} />
-        <Route path="/kanban" element={<KanbanShowcase />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/cta" element={<CTA />} /> */}
